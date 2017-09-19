b7875c661215d22b266d936f37f5b133fb87319a
Shadab Khan <coolshad12@gmail.com>
2017-09-19 15:56:32 +0530

    Add files via upload

---
+++ src/main/java/com/justjava/utils/KeyName.java
@@ -0,0 +1,16 @@
+package com.justjava.utils;
+
+public class KeyName {
+
+	public static final String LOGIN_USER = "user";
+	
+	public static final String CATEGORY_NAME = "categoryName";
+	public static final String ALL_CATEGORIES = "allCategories";
+	
+	public static final String ALL_MANUFACTURERS = "allManufacturers";
+	
+	public static final String ALL_PRODUCTS = "allProducts";
+
+	public static final String SELECTED_PRODUCT="selectedProduct";
+
+}
---
+++ src/main/java/com/justjava/utils/PageName.java
@@ -0,0 +1,30 @@
+package com.justjava.utils;
+
+public class PageName {
+
+	public static final String DASHBORAD = "dashboard";
+	public static final String LOGIN = "login";
+	public static final String REGISTRATION = "registration";
+
+	// Category related
+	public static final String ADD_CATEGORY = "addCategory";
+	public static final String VIEW_EDIT_DELETE_CATEGORY = "viewEditDeleteCategory";
+	
+	// Product related
+	public static final String ADD_MANUFACTURER = "addManufacturer";
+	public static final String VIEW_EDIT_DELETE_MANUFACTURER = "viewEditDeleteManufacturer";
+
+	// Product related
+	public static final String ADD_PRODUCT = "addProduct";
+	public static final String EDIT_PRODUCT = "editProduct";
+	public static final String VIEW_EDIT_DELETE_PRODUCT = "viewEditDeleteProduct";
+
+	// Customer related.
+	public static final String ADD_CUSTOMER = "addCustomer";
+	public static final String EDIT_DELETE_CUSTOMER = "viewEditDeleteCustomer";
+
+	// Purchase related.
+	public static final String ADD_PURCHASE = "addPurchase";
+	public static final String UPDATE_PURCHASE = "updatePurchase";
+
+}
